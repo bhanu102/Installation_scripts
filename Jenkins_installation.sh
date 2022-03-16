7d9c89acf147fd6d74f22872d76bfca03852bfc8
bhanu102 <75179972+bhanu102@users.noreply.github.com>
2022-03-16 10:38:56 +0530

    Update Jenkins_installation.sh

--- Jenkins_installation.sh
+++ Jenkins_installation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt update
-sudo apt install openjdk-8-jdk -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins -y
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+sudo apt install default-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo service jenkins start
+sudo service jenkins status
 #sudo cat /var/lib/jenkins/secrets/initialAdminPassword
